55210617e362e8ce4337436c6b060bb95f183128
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-21 22:55:32 +0300

    ReadRepository Interface generic methods

---
+++ OnionAPI/Infrastructure/OnionAPI.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using OnionAPI.Application.Interfaces.Repositories;
+using OnionAPI.Domain.Common;
+using OnionAPI.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionAPI.Persistence.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : class, IEntityBase, new()
+    {
+        
+
+        public Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, bool enableTracking = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> GetAllByPagingAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, bool enableTracking = false, int currentPage = 1, int pageSize = 3)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> GetAsync(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null, bool enableTracking = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
