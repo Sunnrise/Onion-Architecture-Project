364fc4184fbb80926e2d2575db316f3faf61763a
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-29 15:35:59 +0300

    Cache test with Bogus

---
+++ OnionAPI/Core/OnionAPI.Application/Features/Brands/Commands/CreateBrand/CreateBrandCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Bogus;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using OnionAPI.Application.Bases;
+using OnionAPI.Application.Interfaces.AutoMapper;
+using OnionAPI.Application.Interfaces.UnitOfWorks;
+using OnionAPI.Domain.Entities;
+
+namespace OnionAPI.Application.Features.Brands.Commands.CreateBrand
+{
+    public class CreateBrandCommandHandler : BaseHandler, IRequestHandler<CreateBrandCommandRequest, Unit>
+    {
+        public CreateBrandCommandHandler(IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+        }
+
+
+        public async Task<Unit> Handle(CreateBrandCommandRequest request, CancellationToken cancellationToken)
+        {
+            Faker faker = new Faker();
+            List<Brand> brands = new();
+            for (int i = 0; i < 1000000; i++)
+            {
+                brands.Add(new(faker.Commerce.Department(1)));
+            }
+            await unitOfWork.GetWriteRepository<Brand>().AddRangeAsync(brands);
+            await unitOfWork.SaveAsync();
+            return Unit.Value;
+        }
+    }
+}
